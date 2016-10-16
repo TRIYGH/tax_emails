1e5341def02c07a70f35a79502f91dac50bd5e6c
T. Robert Ward <trironyard@icloud.com>
2016-10-16 08:58:10 -0400

    create program and test with sample of his files

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ file in order to retrieve their email addresses in order to:
 
 1.  Provide the email addresses in a list form
 2.  Compare them to what the CPA has in his MS Outlook Contacts
-3.  Potentiall send out:
+3.  Potential send out:
   a.  Year end planning offering
   b.  Happy Birthday notices.
+4.  Maybe send out confirmation emails to see if it is still a valid email address
+
+The data files are created by a popular tax software program and stored on his local server, BUT are
+  somewhat scrambled so as to prevent easy extraction.  This created some challenge, of course.
---
+++ find_emails.py
@@ -0,0 +1,41 @@
+import glob
+
+
+files = glob.glob('Data/*/*.{}'.format('XX15'))
+
+email_addresses = []
+
+for file in files:
+    data = ""
+    keep_going = True
+    with open(file, encoding='utf-8', errors='ignore') as f:
+        data = f.read()
+    print("##################", len(data))
+    if 'Email' in data:
+        skip = 0
+        how_many = data.count('Email')
+        print(how_many, "HOW  MANY")
+        for each in range(how_many):
+            this_email = ""
+            pos = data.index('Email', skip)
+            skip = pos + 5
+            print(skip)
+            letter_place = pos + 11
+            print(letter_place)
+            counter = 1
+            keep_going = True
+            while keep_going:
+                this_email += data[letter_place]
+                letter_place += 2
+                counter += 1
+                # print(letter_place, counter, keep_going, email_addresses)
+                if counter > 4:
+                    if this_email[-4:] == '.com':
+                        keep_going = False
+                        print("****************", email_addresses, each, "*****************")
+                        email_addresses.append(this_email)
+                if counter > 150:
+                    keep_going = False
+print(email_addresses)
+
+#Email\x08\x00.\x00\x00\x00j\x00k\x00i\x00l\x00p\x00a\x00t\x00r\x00i\x00c\x00k\x00m\x00d\x00@\x00n\x00c\x00.\x00r\x00r\x00.\x00c\x00o\x00m
